134bd5d9709c63738df46d3259da74a43f1e5bbc
Jarvis <mayurshedmekhe10@gmail.com>
2023-01-10 21:14:22 +0530

    Revert "dropdown and form changed"
    This reverts commit 2e3a7045378fabd5d31742cd60f4bc508ec40bd1.

--- public/javascripts/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('search-button').addEventListener('click', function() {
-    var dropdown1Value = document.getElementById('dropdown1').value;
-    var dropdown2Value = document.getElementById('dropdown2').value;
-    var dropdown3Value = document.getElementById('dropdown3').value;
-  
-    console.log('Dropdown 1 value: ' + dropdown1Value);
-    console.log('Dropdown 2 value: ' + dropdown2Value);
-    console.log('Dropdown 3 value: ' + dropdown3Value);
-  });
